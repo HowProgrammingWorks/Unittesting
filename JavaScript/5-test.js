ac4a8261be35e0b620da30f444597bc1fa452fa1
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-02-22 23:43:42 +0200

    Let it throw

--- JavaScript/5-test.js
+++ JavaScript/5-test.js
@@ -2,13 +2,13 @@
 
 const assert = require('assert');
 
-const id = x => x;
 const square = x => x * x;
+const id = x => x;
 
 const value = 10;
 
 const result1 = square(value);
 assert.notStrictEqual(value, result1);
 
-// const result2 = id(value);
-// assert.notStrictEqual(value, result2);
+const result2 = id(value);
+assert.notStrictEqual(value, result2);
